f57e9302ce591adbd8b0827d03a241cf77d1941d
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-03 22:38:37 +0300

    [add] tailwind and navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import {Link} from  "react-router-dom";
+
+function Navbar(){
+    return(
+        <>
+        <nav className="flex justify-center space-x-7 pt-8 bg-indigo-500 h-24">
+            <Link to={"/"} exact="true">Home</Link>
+            <Link to={"/about"} exact="true">About</Link>
+            <Link to={"/review"} exact="true">Reviews</Link>
+        </nav>
+        </>
+    )
+    
+}
+export default Navbar
