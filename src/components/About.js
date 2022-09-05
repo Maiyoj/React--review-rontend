93444e4d28ce8abefbe84f47f0e899ed4eb17306
Maiyoj <valentinemaiyo1995@gmail.com>
2022-09-05 09:56:34 +0300

    [update] about us

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,14 @@ function About(){
 return(
     <>
     <div className="about">
-    <img src={process.env.PUBLIC_URL + '/images/res1.jpeg'} alt="Valentine" className='image1'></img>
-        <div>
+    <div className="img">
+    <img src={process.env.PUBLIC_URL + '/images/img2.jpg'} alt="Valentine" className='image1'></img>
+    </div>
+        <div className="content">
+            <p>Help others make better choices</p>
+            <p>Review your favourite Restaurants</p>
+            <p>Help others make better choices</p>
+            <p>Review your favourite Restaurants</p>
             <p>Help others make better choices</p>
             <p>Review your favourite Restaurants</p>
         </div>
